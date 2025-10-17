081c93edfc0dfa0850ede562a42c202f1520db8c
anunirmani <anuttaranirmani@gmail.com>
2025-10-17 16:56:22 +0530

    Fix thw publications part

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Publications = () => {
+  const [activeFilter, setActiveFilter] = useState('all')
+
   const publications = [
-    { id: 1, title: 'Daily Lankadeepa', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-card', link: 'http://www.lankadeepa.lk/' },
-    { id: 2, title: 'Daily Mirror', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-card', link: 'http://www.dailymirror.lk/' },
-    { id: 3, title: 'Sunday Lankadeepa', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: 'http://www.lankadeepa.lk/sunday' },
-    { id: 4, title: 'Deshaya', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: 'http://www.deshaya.lk/' },
-    { id: 5, title: 'FT', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: 'http://www.ft.lk/' },
-    { id: 6, title: 'Ada', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-card', link: 'http://www.ada.lk/' },
-    { id: 7, title: 'Tamil Mirror', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-card', link: 'http://www.tamilmirror.lk' },
-    { id: 8, title: 'Sunday Times', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: 'http://www.sundaytimes.lk/' },
-    { id: 9, title: 'Hi Magazine', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-web', link: 'http://www.hi.lk/' },
-    { id: 10, title: 'Lanka Women', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-web', link: 'http://www.lw.lk/' },
-    { id: 11, title: 'Sirikatha', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: 'http://sirikatha.lankadeepa.lk/' },
-    { id: 12, title: 'Wijaya', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: 'http://www.wijeya.lk/' },
-    { id: 13, title: 'Bilindu', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: '' },
-    { id: 14, title: 'Pariganaka', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-web', link: '' },
-    { id: 15, title: 'Easy Guide', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-web', link: '' },
-    { id: 16, title: 'Braille', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', filter: 'filter-app', link: '' },
+    { id: 1, title: 'Daily Lankadeepa', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'daily', link: 'http://www.lankadeepa.lk/' },
+    { id: 2, title: 'Daily Mirror', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'daily', link: 'http://www.dailymirror.lk/' },
+    { id: 3, title: 'Sunday Lankadeepa', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'weekly', link: 'http://www.lankadeepa.lk/sunday' },
+    { id: 4, title: 'Deshaya', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'weekly', link: 'http://www.deshaya.lk/' },
+    { id: 5, title: 'Weekend FT', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'weekly', link: 'http://www.ft.lk/' },
+    { id: 6, title: 'Ada', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'daily', link: 'http://www.ada.lk/' },
+    { id: 7, title: 'Tamil Mirror', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'daily', link: 'http://www.tamilmirror.lk' },
+    { id: 8, title: 'Sunday Times', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'weekly', link: 'http://www.sundaytimes.lk/' },
+    { id: 9, title: 'Hi Magazine', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: 'http://www.hi.lk/' },
+    { id: 10, title: 'Lanka Women', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: 'http://www.lw.lk/' },
+    { id: 11, title: 'Sirikatha', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: 'http://sirikatha.lankadeepa.lk/' },
+    { id: 12, title: 'Wijaya', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: 'http://www.wijeya.lk/' },
+    { id: 13, title: 'Bilindu', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: '' },
+    { id: 14, title: 'Pariganaka', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: '' },
+    { id: 15, title: 'Easy Guide', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: '' },
+    { id: 16, title: 'Braille', image: 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=400&h=500&fit=crop', category: 'magazines', link: '' },
   ]
+
+  const handleFilterClick = (filter) => {
+    setActiveFilter(filter)
+  }
+
+  const filteredPublications = activeFilter === 'all' 
+    ? publications 
+    : publications.filter(pub => pub.category === activeFilter)
 
   return (
     <section id="portfolio" className="portfolio">
@@ -31,15 +41,39 @@ const Publications = () => {
         </div>
 
         <ul id="portfolio-flters" className="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-          <li data-filter="*" className="filter-active">All</li>
-          <li data-filter=".filter-app">Weekly</li>
-          <li data-filter=".filter-card">Daily</li>
-          <li data-filter=".filter-web">Magazines</li>
+          <li 
+            className={activeFilter === 'all' ? 'filter-active' : ''} 
+            onClick={() => handleFilterClick('all')}
+            style={{ cursor: 'pointer' }}
+          >
+            All
+          </li>
+          <li 
+            className={activeFilter === 'weekly' ? 'filter-active' : ''} 
+            onClick={() => handleFilterClick('weekly')}
+            style={{ cursor: 'pointer' }}
+          >
+            Weekly
+          </li>
+          <li 
+            className={activeFilter === 'daily' ? 'filter-active' : ''} 
+            onClick={() => handleFilterClick('daily')}
+            style={{ cursor: 'pointer' }}
+          >
+            Daily
+          </li>
+          <li 
+            className={activeFilter === 'magazines' ? 'filter-active' : ''} 
+            onClick={() => handleFilterClick('magazines')}
+            style={{ cursor: 'pointer' }}
+          >
+            Magazines
+          </li>
         </ul>
 
         <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-          {publications.map((pub) => (
-            <div className={`col-lg-3 col-md-6 portfolio-item ${pub.filter}`} key={pub.id}>
+          {filteredPublications.map((pub) => (
+            <div className="col-lg-3 col-md-6 portfolio-item" key={pub.id}>
               <div className="portfolio-img">
                 <img src={pub.image} className="img-fluid" alt={pub.title} />
               </div>
